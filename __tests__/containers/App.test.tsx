599455851cc819ac388b0168467d85b1adfba3ef
piny940 <an.s.shoma@gmail.com>
2023-07-01 02:15:52 +0900

    PieItemのテストを作成

--- __tests__/containers/App.test.tsx
+++ __tests__/containers/App.test.tsx
@@ -1,14 +1,13 @@
-import { App } from '@/containers/App'
-import { TestID } from '@/resources/TestID'
+import { Index } from '@/containers/Index'
 import { expect } from '@jest/globals'
 import { render, waitFor } from '@testing-library/react'
 
-describe('<App />', () => {
-  it('Appが正常に描画される', async () => {
-    const { getByTestId } = render(<App />)
+describe('<Index />', () => {
+  it('Indexが正常に描画される', async () => {
+    const component = render(<Index />)
 
     await waitFor(() => {
-      expect(getByTestId(TestID.APP)).toBeTruthy()
+      expect(component).toBeTruthy()
     })
   })
 })
