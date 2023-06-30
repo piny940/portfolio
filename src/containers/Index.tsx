ce6efb642462a9c33d2dcc8de837b2102ce6e7e3
piny940 <an.s.shoma@gmail.com>
2023-07-01 00:17:27 +0900

    Fix

--- src/containers/Index.tsx
+++ src/containers/Index.tsx
@@ -5,18 +5,16 @@ export const Index: React.FC = () => {
   return (
     <div id="index" className="container">
       <div className="d-flex flex-column align-items-center">
-        <div className="d-flex flex-column align-items-center">
-          <div className="top-middle d-flex flex-column align-items-center">
-            <Image
-              src="/images/icon.png"
-              alt="icon"
-              width={150}
-              height={150}
-              className="rounded-circle"
-            />
-            <h1 className="mt-2">Piny940</h1>
-            <p className="mt-3 top-profile">{profileData.frontProfile}</p>
-          </div>
+        <div className="top-middle d-flex flex-column align-items-center">
+          <Image
+            src="/images/icon.png"
+            alt="icon"
+            width={150}
+            height={150}
+            className="rounded-circle"
+          />
+          <h1 className="mt-2">Piny940</h1>
+          <p className="mt-3 top-profile">{profileData.frontProfile}</p>
         </div>
       </div>
     </div>
