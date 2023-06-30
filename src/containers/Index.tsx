3f289393b312c7e98cac42d7903bdd8966a15f0d
piny940 <an.s.shoma@gmail.com>
2023-07-01 00:13:39 +0900

    トップのプロフィールを作成

--- src/containers/Index.tsx
+++ src/containers/Index.tsx
@@ -1,18 +1,22 @@
+import { profileData } from '@/data/profile'
 import Image from 'next/image'
 
 export const Index: React.FC = () => {
   return (
     <div id="index" className="container">
       <div className="d-flex flex-column align-items-center">
-        <div className="d-flex flex-column">
-          <Image
-            src="/images/icon.png"
-            alt="icon"
-            width={150}
-            height={150}
-            className="rounded-circle"
-          />
-          <p className="mt-3">ほげほげほげほげほげ</p>
+        <div className="d-flex flex-column align-items-center">
+          <div className="top-middle d-flex flex-column align-items-center">
+            <Image
+              src="/images/icon.png"
+              alt="icon"
+              width={150}
+              height={150}
+              className="rounded-circle"
+            />
+            <h1 className="mt-2">Piny940</h1>
+            <p className="mt-3 top-profile">{profileData.frontProfile}</p>
+          </div>
         </div>
       </div>
     </div>
