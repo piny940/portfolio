930999ae612fe122bc5e3f6ffa8371aa98feb3ff
piny940 <an.s.shoma@gmail.com>
2023-07-02 14:15:23 +0900

    profileを作成

--- src/containers/Index.tsx
+++ src/containers/Index.tsx
@@ -1,23 +1,10 @@
-import { profileData } from '@/data/profile'
-import Image from 'next/image'
 import { Skills } from './Skills'
+import { Profiles } from './Profile'
 
 export const Index: React.FC = () => {
   return (
     <div id="index">
-      <div className="d-flex flex-column align-items-center">
-        <div className="top-middle d-flex flex-column align-items-center">
-          <Image
-            src="/images/icon.png"
-            alt="icon"
-            width={150}
-            height={150}
-            className="rounded-circle"
-          />
-          <h1 className="mt-2">Piny940</h1>
-          <p className="mt-3 top-profile">{profileData.frontProfile}</p>
-        </div>
-      </div>
+      <Profiles />
       <Skills className="bg-body-tertiary" />
     </div>
   )
---
+++ src/containers/Profile.tsx
@@ -0,0 +1,39 @@
+import { profileData } from '@/data/profile'
+import Image from 'next/image'
+import styled from 'styled-components'
+
+const BgDiv = styled.div`
+  background-image: url('/images/profile/background.png');
+  background-size: cover;
+  opacity: 0.3;
+`
+
+const ProfileDiv = styled.div`
+  border-top-width: 10px;
+  border-style: solid;
+  border-image: linear-gradient(
+    to right,
+    rgb(48, 117, 255) 0%,
+    rgb(45, 219, 208) 100%
+  );
+  border-image-slice: 1;
+`
+
+export const Profiles: React.FC = () => {
+  return (
+    <div className="d-flex flex-column align-items-center position-relative">
+      <BgDiv className="position-absolute w-100 h-100"></BgDiv>
+      <ProfileDiv className="top-middle d-flex flex-column align-items-center position-relative bg-body rounded p-3">
+        <Image
+          src="/images/icon.png"
+          alt="icon"
+          width={150}
+          height={150}
+          className="rounded-circle"
+        />
+        <h1 className="mt-2">Piny940</h1>
+        <p className="mt-3 top-profile">{profileData.frontProfile}</p>
+      </ProfileDiv>
+    </div>
+  )
+}
