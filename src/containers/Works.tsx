43acb629e1e951a28021d1bd705f2cd0d3f8d4ee
piny940 <an.s.shoma@gmail.com>
2023-07-02 19:28:00 +0900

    titleに下線をひく

--- src/containers/Works.tsx
+++ src/containers/Works.tsx
@@ -5,7 +5,7 @@ export type WorksProps = {
 export const Works: React.FC<WorksProps> = ({ className = '' }) => {
   return (
     <div className={'d-flex flex-column align-items-center p-5 ' + className}>
-      <h1 className="text-center">Works</h1>
+      <h1 className="text-center title-underline">Works</h1>
       <div className="row row-cols-md-2 w-75 mt-4">ほげ</div>
     </div>
   )
