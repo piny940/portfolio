1a4015330c8faf69f3b6b41b47670056ac9adc23
piny940 <an.s.shoma@gmail.com>
2023-07-02 22:27:00 +0900

    githubへのリンクを貼る

--- src/components/Portfolio/WorkItem.tsx
+++ src/components/Portfolio/WorkItem.tsx
@@ -2,6 +2,8 @@ import { WorkType } from '@/resources/types'
 import styled from 'styled-components'
 import { MaterialIcon } from '../Common/MaterialIcon'
 import Link from 'next/link'
+import { useTheme } from '@/context/ThemeProvider'
+import Image from 'next/image'
 
 const WorkItemDiv = styled.div`
   min-width: 300px;
@@ -13,6 +15,8 @@ export type WorkItemProps = {
 }
 
 export const WorkItem: React.FC<WorkItemProps> = ({ work, className = '' }) => {
+  const { theme } = useTheme()
+
   return (
     <WorkItemDiv
       className={
@@ -20,9 +24,21 @@ export const WorkItem: React.FC<WorkItemProps> = ({ work, className = '' }) => {
       }
     >
       <h3 className="h5 my-1">{work.title}</h3>
-      <p className="mt-2 mb-0">
+      <p className="mt-2 mb-0 d-flex align-items-center">
+        <Link target="_blank" href={work.github} className="mx-1">
+          <Image
+            src={
+              theme === 'light'
+                ? '/images/common/github.png'
+                : '/images/common/github-white.png'
+            }
+            width={31}
+            height={31}
+            alt="github-icon"
+          />
+        </Link>
         {work.link && (
-          <Link href={work.link}>
+          <Link href={work.link} target="_blank" className="mx-1">
             <MaterialIcon name="share" />
           </Link>
         )}
