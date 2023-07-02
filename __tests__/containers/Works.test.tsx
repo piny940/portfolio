c0b130ea7ece3ec3b9c9faa99b1f5e18b29c84e7
piny940 <an.s.shoma@gmail.com>
2023-07-02 22:51:12 +0900

    Worksのテストを作成

---
+++ __tests__/containers/Works.test.tsx
@@ -0,0 +1,14 @@
+import { Works, WorksProps } from '@/containers/Works'
+import { expect } from '@jest/globals'
+import { render, waitFor } from '@testing-library/react'
+import { Mock } from 'ts-mockery'
+
+describe('<Works />', () => {
+  it('正常に描画される', async () => {
+    const props = Mock.from<WorksProps>({})
+    const component = render(<Works {...props} />)
+    await waitFor(() => {
+      expect(component).toBeTruthy()
+    })
+  })
+})
