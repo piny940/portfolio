4257e933065dfa2fc9ab09726f0b1ee450ace283
piny940 <an.s.shoma@gmail.com>
2023-07-05 00:37:29 +0900

    プロジェクト画像を配置

--- src/containers/Project.tsx
+++ src/containers/Project.tsx
@@ -14,13 +14,14 @@ export const ProjectShow: React.FC<ProjectShowProps> = ({ title }) => {
     <div className="wrapper mx-auto mt-3">
       <h1 className="title-underline">{title}</h1>
       <div className="row">
-        <div className="col-lg-6 position-relative">
+        <div className="col-lg-6">
           {project?.imageSrc && (
             <Image
-              alt="image"
-              src={project?.imageSrc}
-              width={400}
-              height={200}
+              src={project.imageSrc}
+              alt="project image"
+              style={{ width: '100%', height: 'auto' }}
+              width={1920}
+              height={1080}
             />
           )}
         </div>
