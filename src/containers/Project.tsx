f8c4f102f78867013ab13b9a4f04ac0296047cbc
piny940 <an.s.shoma@gmail.com>
2023-07-26 22:40:03 +0900

    リンクを画像下に配置するようにする

--- src/containers/Project.tsx
+++ src/containers/Project.tsx
@@ -1,8 +1,13 @@
+import { useTheme } from '@/context/ThemeProvider'
 import { projectsData } from '@/data/projects'
 import Error from 'next/error'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
+import githubWhiteIcon from '../resources/images/common/github-white.png'
+import githubIcon from '../resources/images/common/github.png'
+import { MaterialIcon } from '@/components/Common/MaterialIcon'
+import Link from 'next/link'
 
 export type ProjectShowProps = {
   title: string
@@ -11,6 +16,7 @@ export type ProjectShowProps = {
 export const ProjectShow: React.FC<ProjectShowProps> = ({ title }) => {
   const project = projectsData.find((v) => v.title === title)
   const [detail, setDetail] = useState('')
+  const { theme } = useTheme()
 
   const loadDetail = async () => {
     if (!project?.detailSrc) return
@@ -28,10 +34,12 @@ export const ProjectShow: React.FC<ProjectShowProps> = ({ title }) => {
     <div className="wrapper mx-auto mt-3">
       <h1 className="title-underline">{title}</h1>
       <div className="row">
-        <div className="col-lg-6">
-          <ReactMarkdown>{detail}</ReactMarkdown>
+        <div className="col-lg-8">
+          <div className="px-2">
+            <ReactMarkdown>{detail}</ReactMarkdown>
+          </div>
         </div>
-        <div className="col-lg-6">
+        <div className="col-lg-4">
           {project?.imageSrc && (
             <Image
               src={project.imageSrc}
@@ -41,6 +49,33 @@ export const ProjectShow: React.FC<ProjectShowProps> = ({ title }) => {
               height={1080}
             />
           )}
+          <div className="my-2 mx-3">
+            <ul className="list-unstyled d-flex justify-content-center">
+              <li className="mx-1">
+                {project.github && (
+                  <Link target="_blank" href={project.github} className="mx-1">
+                    <Image
+                      src={theme === 'light' ? githubIcon : githubWhiteIcon}
+                      width={31}
+                      height={31}
+                      alt="github-icon"
+                    />
+                  </Link>
+                )}
+              </li>
+              <li className="mx-1">
+                {project.link && (
+                  <Link
+                    href={project.link}
+                    target="_blank"
+                    className="text-body"
+                  >
+                    <MaterialIcon name="share" />
+                  </Link>
+                )}
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
