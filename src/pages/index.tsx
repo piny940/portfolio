58492bc4c3c08536265b5f40d716757f788ce612
piny940 <an.s.shoma@gmail.com>
2023-09-28 09:36:12 +0900

    TechnologyLoaderを動作確認

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,25 +5,35 @@ import { YamlLoader } from '@/loader/_common'
 import { ProjectsLoader } from '@/loader/projects'
 import { ProfileLoader } from '@/loader/profile'
 import { ProfileData } from '@/models/profile'
+import { TechnologiesData } from '@/models/technology'
+import { TechnologiesLoader } from '@/loader/technologies'
 
 type HomeProps = {
   profileData: ProfileData
   projectsData: ProjectsData
+  technologiesData: TechnologiesData
 }
 
 export const getStaticProps = async (): Promise<{ props: HomeProps }> => {
   const yamlLoader = new YamlLoader()
   const projectsData = new ProjectsLoader(yamlLoader).load()
   const profileData = new ProfileLoader(yamlLoader).load()
+  const technologiesData = new TechnologiesLoader(yamlLoader).load()
   return {
     props: {
       projectsData: projectsData,
       profileData: profileData,
+      technologiesData: technologiesData,
     },
   }
 }
 
-const Home: NextPage<HomeProps> = ({ projectsData, profileData }) => {
+const Home: NextPage<HomeProps> = ({
+  projectsData,
+  profileData,
+  technologiesData,
+}) => {
+  console.log(technologiesData)
   return <Index />
 }
 
