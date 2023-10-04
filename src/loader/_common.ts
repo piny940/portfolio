6b26ca083e01b445774cbb15ff106a463356c20c
mikan <83708535+piny940@users.noreply.github.com>
2023-10-04 21:39:38 +0900

    FileLoaderを作成

--- src/loader/_common.ts
+++ src/loader/_common.ts
@@ -10,3 +10,11 @@ export class YamlLoader implements IYamlLoader {
     return load(readFileSync(filename, 'utf8')) as T
   }
 }
+
+export interface IFileLoader {
+  load: (filename: string) => string
+}
+
+export class FileLoader implements IFileLoader {
+  load = (filename: string) => readFileSync(filename, 'utf8')
+}
