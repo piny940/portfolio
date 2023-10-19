16ba0e787e5603bc649d6bc3ef3f7f55ceb5c909
mikan <83708535+piny940@users.noreply.github.com>
2023-10-19 11:56:53 +0900

    不要な文言を削除

--- src/containers/Projects.tsx
+++ src/containers/Projects.tsx
@@ -16,7 +16,6 @@ export const ProjectsIndex: React.FC<ProjectsIndexProps> = ({
       className={'d-flex flex-column align-items-center py-5 ' + className}
     >
       <h2 className="h1 text-center title-underline">Projects</h2>
-      <p className="mt-4">それぞれQiita記事へのリンクになっています</p>
       <div className="row row-cols-md-2 row-cols-xl-3 w-75 d-flex align-items-stretch">
         {projects.sortedByFavorite().map((project) => (
           <div className="col-md p-3 my-3" key={project.getTitle()}>
