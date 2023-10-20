049ce71de8d8344834525d53c3b879c99cdc5ba8
piny940 <an.s.shoma@gmail.com>
2023-10-20 19:41:44 +0900

    技術スタックページを作成

--- src/containers/Skills.tsx
+++ src/containers/Skills.tsx
@@ -1,14 +1,31 @@
 import { PortfolioData } from '@/controllers/data_controller'
 import { memo } from 'react'
+import { PortfolioController } from '@/controllers/portfolio_controller'
+import { SkillItem } from '@/components/Portfolio/SkillItem'
 
 export type SkillsProps = {
   data: PortfolioData
 }
 
 const Skills = ({ data }: SkillsProps): JSX.Element => {
+  const controller = new PortfolioController(data)
+
   return (
     <div className="container mt-3">
       <h1 className="title-underline">技術スタック</h1>
+      <ul className="list-unstyled row row-cols-md-3 mt-4">
+        {controller
+          .getTechStacks()
+          .sortedByProficiency()
+          .map((techStack) => (
+            <li
+              className="col p-3 my-3"
+              key={techStack.getTechnology().getName()}
+            >
+              <SkillItem techStack={techStack} />
+            </li>
+          ))}
+      </ul>
     </div>
   )
 }
