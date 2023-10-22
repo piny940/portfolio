e99afe9848f4db660f29415acc830da45db7415f
piny940 <an.s.shoma@gmail.com>
2023-10-23 00:31:27 +0900

    ProjectItemsをProjectsで使用

--- src/containers/Projects.tsx
+++ src/containers/Projects.tsx
@@ -1,7 +1,7 @@
 import { PortfolioData } from '@/controllers/data_controller'
 import { PortfolioController } from '@/controllers/portfolio_controller'
-import { ProjectItem } from '@/components/Portfolio/ProjectItem'
 import { memo } from 'react'
+import { ProjectItems } from '@/components/Portfolio/ProjectItems'
 
 export type ProjectsProps = {
   data: PortfolioData
@@ -16,16 +16,7 @@ const Projects = ({ data }: ProjectsProps): JSX.Element => {
       className="d-flex flex-column align-items-center py-5"
     >
       <h2 className="h1 text-center title-underline">プロジェクト</h2>
-      <div className="row row-cols-md-2 row-cols-xl-3 w-75 d-flex align-items-stretch">
-        {controller
-          .getProjects()
-          .sortedByFavorite()
-          .map((project) => (
-            <div className="col-md p-3 my-3" key={project.getTitle()}>
-              <ProjectItem project={project} />
-            </div>
-          ))}
-      </div>
+      <ProjectItems projects={controller.getProjects().sortedByFavorite()} />
     </section>
   )
 }
