d1a8407040426880f1319d1abbdb1c012cf5247f
piny940 <an.s.shoma@gmail.com>
2023-10-23 01:35:37 +0900

    スキルごとにプロジェクト一覧を表示

--- src/containers/Skill.tsx
+++ src/containers/Skill.tsx
@@ -11,22 +11,24 @@ export type SkillProps = {
 
 const Skill = ({ data, id }: SkillProps): JSX.Element => {
   const controller = new PortfolioController(data)
-  const skill = useMemo(
+  const techStack = useMemo(
     () => controller.getTechStacks().findByTechnologyId(id),
     [id, data]
   )
+  const technology = useMemo(() => techStack?.getTechnology(), [techStack])
 
-  if (!skill) return <Error statusCode={404} />
+  if (!techStack || !technology) return <Error statusCode={404} />
   return (
     <div className="container mt-3">
-      <h1 className="title-underline ps-2">
-        {skill.getTechnology().getName()}
-      </h1>
+      <h1 className="title-underline ps-2">{technology.getName()}</h1>
       <section className="py-3 px-5">
         <h2>プロジェクト一覧</h2>
         <div className="d-flex flex-column align-items-center">
           <ProjectItems
-            projects={controller.getProjects().sortedByFavorite()}
+            projects={controller
+              .getProjects()
+              .filterByTechnology(technology.getId())
+              .sortedByFavorite()}
           />
         </div>
       </section>
