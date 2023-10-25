f3efaf53f1492a4370401cd48b23a07b69502e36
piny940 <an.s.shoma@gmail.com>
2023-10-25 21:27:09 +0900

    Blogsを追加

--- src/controllers/portfolio_controller.ts
+++ src/controllers/portfolio_controller.ts
@@ -3,22 +3,26 @@ import { Projects } from '@/models/project'
 import { TechStacks } from '@/models/tech_stack'
 import { Technologies } from '@/models/technology'
 import { PortfolioData } from './data_controller'
+import { Blogs } from '@/models/blogs'
 
 export class PortfolioController {
   #profile: Profile
   #projects: Projects
   #technologies: Technologies
   #techStacks: TechStacks
+  #blogs: Blogs
 
   constructor(data: PortfolioData) {
     this.#profile = new Profile(data.profileData)
     this.#technologies = new Technologies(data.technologiesData)
     this.#projects = new Projects(data.projectsData, this.#technologies)
     this.#techStacks = new TechStacks(data.techStacksData, this.#technologies)
+    this.#blogs = new Blogs(data.blogsData, this.#technologies)
   }
 
   getProfile = () => this.#profile
   getProjects = () => this.#projects
   getTechnologies = () => this.#technologies
   getTechStacks = () => this.#techStacks
+  getBlogs = () => this.#blogs
 }
