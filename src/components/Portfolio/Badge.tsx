fa95d8cddba35f89d7929f2453afb491d99c732a
piny940 <an.s.shoma@gmail.com>
2023-10-25 22:00:23 +0900

    Badgeを作成

---
+++ src/components/Portfolio/Badge.tsx
@@ -0,0 +1,26 @@
+import { CSSProperties, memo } from 'react'
+import styled from 'styled-components'
+
+const BadgeSpan = styled.span`
+  font-size: 17px;
+`
+
+export type BadgeProps = {
+  color: string
+  label: string
+  size: number
+}
+
+const Badge = ({ color, label, size }: BadgeProps): JSX.Element => {
+  const style: CSSProperties = {
+    color: color,
+    fontSize: `${size}px`,
+  }
+  return (
+    <BadgeSpan style={style} className="badge bg-primary">
+      {label}
+    </BadgeSpan>
+  )
+}
+
+export default memo(Badge)
