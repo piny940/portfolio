ecf23d4f5b193e196fb20956961f8852b95f4cc0
piny940 <an.s.shoma@gmail.com>
2023-10-25 21:57:12 +0900

    Skillsのテストを追加

--- src/containers/Skills.tsx
+++ src/containers/Skills.tsx
@@ -22,17 +22,14 @@ const Skills = ({ data }: SkillsProps): JSX.Element => {
       <div className="container mb-5 d-flex align-items-center flex-column">
         <h1 className="h1 text-center title-underline">技術スタック</h1>
         <ul className="list-unstyled row row-cols-md-3 mt-4">
-          {controller
-            .getTechStacks()
-            .sortedByProficiency()
-            .map((techStack) => (
-              <li
-                className="col p-3 my-3"
-                key={techStack.getTechnology().getName()}
-              >
-                <SkillItem techStack={techStack} />
-              </li>
-            ))}
+          {controller.techStacks.sortedByProficiency().map((techStack) => (
+            <li
+              className="col p-3 my-3"
+              key={techStack.getTechnology().getId()}
+            >
+              <SkillItem techStack={techStack} />
+            </li>
+          ))}
         </ul>
       </div>
     </>
