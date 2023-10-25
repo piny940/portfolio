6ae69cb48d72ec534e83627b0220d232c761df78
mikan <83708535+piny940@users.noreply.github.com>
2023-10-25 10:48:30 +0900

    tmp

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,23 +9,16 @@ function Document() {
           rel="stylesheet"
         ></link>
         <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400&display=swap"
           rel="stylesheet"
         ></link>
         <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@500&display=swap"
           rel="stylesheet"
-          integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
-          crossOrigin="anonymous"
-        />
+        ></link>
       </Head>
       <body>
         <Main />
-        <script
-          defer
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
-        ></script>
         <NextScript />
       </body>
     </Html>
