eb94eaeb287996ad6d0c3ba2c5796c4c25bed243
mikan <83708535+piny940@users.noreply.github.com>
2023-10-25 10:28:47 +0900

    bootstrap, google fontをCDNで読み込む

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,9 +8,24 @@ function Document() {
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
           rel="stylesheet"
         ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500&display=swap"
+          rel="stylesheet"
+        ></link>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+          crossOrigin="anonymous"
+        />
       </Head>
       <body>
         <Main />
+        <script
+          defer
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+        ></script>
         <NextScript />
       </body>
     </Html>
