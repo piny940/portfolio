8d5a668d5b6852dfe9e55443715ce164b803f3db
piny940 <an.s.shoma@gmail.com>
2023-12-31 08:54:06 +0900

    webpack

---
+++ blog-updater/webpack.js
@@ -0,0 +1,29 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
+
+module.exports = {
+  plugins: [
+    new CleanWebpackPlugin(),
+    new Dotenv({ systemvars: true }),
+  ],
+  output: {
+    path: `${__dirname}/out`,
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  watchOptions: {
+    ignored: /node_modules/,
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+}
