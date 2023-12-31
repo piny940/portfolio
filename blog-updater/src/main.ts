fbdf253710aba39fb89a79b538e58b6a28d5fb44
piny940 <an.s.shoma@gmail.com>
2023-12-31 13:00:40 +0900

    github apiに接続できるようにする

--- blog-updater/src/main.ts
+++ blog-updater/src/main.ts
@@ -1,8 +1,25 @@
-import { func } from './util'
+import crypto from 'crypto'
+import { Octokit } from 'octokit'
+import { createAppAuth } from '@octokit/auth-app'
 
-const a = {
-  b: 1,
+const appId = process.env.GITHUB_APP_ID as string
+const privateKey = process.env.GITHUB_APP_PRIVATE_KEY as string
+const clientId = process.env.GITHUB_APP_CLIENT_ID as string
+const clientSecret = process.env.GITHUB_APP_CLIENT_SECRET as string
+
+const privateKeyPkcs8 = crypto.createPrivateKey(privateKey).export({
+  type: 'pkcs8',
+  format: 'pem',
+})
+
+const appOctokit = new Octokit({
+  authStrategy: createAppAuth,
+  auth: { appId, privateKey: privateKeyPkcs8, clientId, clientSecret },
+})
+console.log(appOctokit)
+
+const getData = async () => {
+  const data = await appOctokit.rest.apps.getAuthenticated()
+  console.log(data)
 }
-console.log(a)
-
-console.log(func())
+void getData()
