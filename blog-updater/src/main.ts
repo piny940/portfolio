c473734b669c203855a3876015a1980a5f18b554
piny940 <an.s.shoma@gmail.com>
2023-12-31 13:18:01 +0900

    private keyのバグを修正

--- blog-updater/src/main.ts
+++ blog-updater/src/main.ts
@@ -1,9 +1,15 @@
 import crypto from 'crypto'
 import { Octokit } from 'octokit'
 import { createAppAuth } from '@octokit/auth-app'
+import dotenv from 'dotenv'
+
+dotenv.config({ path: '.env.development' })
 
 const appId = process.env.GITHUB_APP_ID as string
-const privateKey = process.env.GITHUB_APP_PRIVATE_KEY as string
+const privateKey = (process.env.GITHUB_APP_PRIVATE_KEY as string).replace(
+  /\\n/g,
+  '\n'
+)
 const clientId = process.env.GITHUB_APP_CLIENT_ID as string
 const clientSecret = process.env.GITHUB_APP_CLIENT_SECRET as string
 
