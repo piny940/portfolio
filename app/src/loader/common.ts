0296bd8a46087a6705638795e21991eabfa70920
piny940 <an.s.shoma@gmail.com>
2024-01-01 19:43:01 +0900

    postgresに接続できるようにする

--- app/src/loader/common.ts
+++ app/src/loader/common.ts
@@ -1,6 +1,16 @@
 import { readFileSync } from 'fs'
 import { IFileLoader, IYamlLoader } from './_common'
 import { load } from 'js-yaml'
+import { Client } from 'pg'
+
+export const dbClient = new Client({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+})
+dbClient.connect()
 
 export class FileLoader implements IFileLoader {
   load = (filename: string) => {
