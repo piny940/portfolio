cd69484ff4668005cfa4c84fd47c739f62c6b6e5
piny940 <an.s.shoma@gmail.com>
2024-01-02 10:50:45 +0900

    .envを設定

--- admin-backend/db/init.go
+++ admin-backend/db/init.go
@@ -18,8 +18,8 @@ var db *DB
 func Init() {
 	c := config.GetConfig()
 	dsn := fmt.Sprintf("user=%s password=%s host=%s dbname=%s sslmode=%s",
-		os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_HOST"),
-		os.Getenv("POSTGRES_NAME"), os.Getenv("POSTGRES_SSLMODE"))
+		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"),
+		os.Getenv("DB_NAME"), os.Getenv("DB_SSLMODE"))
 	client, err := sql.Open(c.GetString("db.provider"), dsn)
 	if err != nil {
 		panic(err)
