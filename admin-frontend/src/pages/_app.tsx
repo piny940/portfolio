3287343cbc4aeea9a7e1e0cf58a6735f0370ac82
piny940 <an.s.shoma@gmail.com>
2024-01-03 15:42:47 +0900

    ThemeProvider, CssBaselineを配置

--- admin-frontend/src/pages/_app.tsx
+++ admin-frontend/src/pages/_app.tsx
@@ -1,15 +1,18 @@
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
+const theme = {}
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
         <meta content="width=device-width,initial-scale=1" name="viewport" />
       </Head>
+      <CssBaseline />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
 
