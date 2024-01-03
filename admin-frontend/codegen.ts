3144c8edc619e0df846898d6573a621565ae46cc
piny940 <an.s.shoma@gmail.com>
2024-01-03 11:57:16 +0900

    typescript-urlqを導入

--- admin-frontend/codegen.ts
+++ admin-frontend/codegen.ts
@@ -8,6 +8,7 @@ const config: CodegenConfig = {
       plugins: [
         'typescript',
         'typescript-operations',
+        'typescript-urql',
       ],
       config: {
         strictScalars: true,
