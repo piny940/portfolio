a28a93eb6f51135b63d6b19ca3dd2e0c8164e18d
piny940 <an.s.shoma@gmail.com>
2024-01-03 10:12:34 +0900

    appをsetup

---
+++ admin-frontend/src/pages/404.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+const Custom404: React.FC = () => {
+  return (
+    <div className="container" >
+      <h1>ページが見つかりませんでした。(404)</h1>
+      <p>
+        <Link href="/">ホームへ戻る</Link>
+      </p>
+    </div>
+  )
+}
+
+export default Custom404
---
+++ admin-frontend/src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import '../styles/globals.scss'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta content="width=device-width,initial-scale=1" name="viewport" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
