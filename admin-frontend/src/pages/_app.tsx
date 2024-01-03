06f83be5d4db9e05291a260054fadbe75fabd94b
piny940 <an.s.shoma@gmail.com>
2024-01-03 16:38:24 +0900

    themeを設定

--- admin-frontend/src/pages/_app.tsx
+++ admin-frontend/src/pages/_app.tsx
@@ -1,10 +1,25 @@
-import { CssBaseline, ThemeProvider } from '@mui/material'
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { Layout } from '@/layouts/Layout'
 
-const theme = {}
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#71f5aa',
+      main: 'rgb(20, 214, 182)',
+      dark: '#389200',
+      contrastText: '#000000',
+    },
+    secondary: {
+      light: '#5557ff',
+      main: '#003eda',
+      dark: '#062f98',
+      contrastText: '#000',
+    },
+  },
+})
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
