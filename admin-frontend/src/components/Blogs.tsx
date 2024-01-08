bf4b33ff4b62e3cab4596effa0287e5c0d7bf5d6
piny940 <an.s.shoma@gmail.com>
2024-01-08 11:27:11 +0900

    Blogsコンポーネントを作成

---
+++ admin-frontend/src/components/Blogs.tsx
@@ -0,0 +1,13 @@
+import { Table, TableBody, TableCell, TableRow } from '@mui/material'
+
+export const Blogs = (): JSX.Element => {
+  return (
+    <Table>
+      <TableBody>
+        <TableRow>
+          <TableCell>ほげ</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  )
+}
