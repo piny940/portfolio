22f511bfe821ce2ac70ce13ab8101fbe5f4760ca
piny940 <an.s.shoma@gmail.com>
2024-01-09 00:20:48 +0900

    Zennを削除

--- admin-frontend/utils/types.ts
+++ admin-frontend/utils/types.ts
@@ -1,6 +1,5 @@
 export const blogKindLabel = {
   0: 'Qiita',
-  1: 'Zenn',
 } as const
 export type BlogKind = keyof typeof blogKindLabel
-export const allBlogKinds = [0, 1] as const
+export const allBlogKinds = [0] as const
