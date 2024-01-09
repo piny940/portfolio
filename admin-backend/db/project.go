2a94b7232e780ad13fbbf3a4712f1e8d94c9c88d
piny940 <an.s.shoma@gmail.com>
2024-01-10 01:02:12 +0900

    projectの編集時のバグを修正

--- admin-backend/db/project.go
+++ admin-backend/db/project.go
@@ -50,6 +50,9 @@ func (r *projectRepo) List() ([]*domain.Project, error) {
 func (r *projectRepo) Update(input domain.ProjectInput) (*domain.Project, error) {
 	var project domain.Project
 	r.db.Client.First(&project, "id = ?", input.ID)
+	project.Title = input.Title
+	project.Description = input.Description
+	project.IsFavorite = input.IsFavorite
 	result := r.db.Client.Save(project)
 	if result.Error != nil {
 		return nil, result.Error
