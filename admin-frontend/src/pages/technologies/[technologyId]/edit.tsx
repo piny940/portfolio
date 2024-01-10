2939d5b2b4ca832ec414d535283c433c263d17ee
piny940 <an.s.shoma@gmail.com>
2024-01-10 13:14:27 +0900

    fix edit

--- admin-frontend/src/pages/technologies/[technologyId]/edit.tsx
+++ admin-frontend/src/pages/technologies/[technologyId]/edit.tsx
@@ -1,19 +1,19 @@
-import { EditBlog } from '@/components/EditBlog'
-import { useGetBlogQuery } from '@/graphql/types'
+import { EditTechnology } from '@/components/EditTechnology'
+import { useGetTechnologyQuery } from '@/graphql/types'
 import Error from 'next/error'
 import { useRouter } from 'next/router'
 
-const EditBlogPage = (): JSX.Element => {
+const EditTechnologyPage = (): JSX.Element => {
   const router = useRouter()
-  const { blogId } = router.query
+  const { technologyId } = router.query
 
-  const [{ data, error }] = useGetBlogQuery({
-    variables: { id: parseInt(blogId as string) || -1 },
+  const [{ data, error }] = useGetTechnologyQuery({
+    variables: { id: parseInt(technologyId as string) || -1 },
   })
 
   if (error) return <Error statusCode={404} />
   if (!data) return <>loading</>
-  return <EditBlog blog={data.blog} />
+  return <EditTechnology technology={data.technology} />
 }
 
-export default EditBlogPage
+export default EditTechnologyPage
