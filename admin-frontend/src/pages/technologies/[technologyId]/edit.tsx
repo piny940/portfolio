4c3517fe50204b57313cab7f9bf5c6f40072c8fa
piny940 <an.s.shoma@gmail.com>
2024-01-10 13:16:26 +0900

    edit画面でrouterがreadyになるまでpauseするようにする

--- admin-frontend/src/pages/technologies/[technologyId]/edit.tsx
+++ admin-frontend/src/pages/technologies/[technologyId]/edit.tsx
@@ -8,7 +8,8 @@ const EditTechnologyPage = (): JSX.Element => {
   const { technologyId } = router.query
 
   const [{ data, error }] = useGetTechnologyQuery({
-    variables: { id: parseInt(technologyId as string) || -1 },
+    variables: { id: parseInt(technologyId as string) },
+    pause: !router.isReady,
   })
 
   if (error) return <Error statusCode={404} />
