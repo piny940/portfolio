3462e91eee7a3ce706b9c79040857b8bdf367ffd
piny940 <an.s.shoma@gmail.com>
2024-01-17 09:53:22 +0900

    debug

--- app/src/utils/api.ts
+++ app/src/utils/api.ts
@@ -1,4 +1,5 @@
 console.log('Backend host:', process.env.BACKEND_HOST)
+console.log('Backend token:', !!process.env.BACKEND_TOKEN)
 export const queryGql = async <T>(query: string) => {
   const response = await fetch(`${process.env.BACKEND_HOST || ''}/v1/query`, {
     method: 'POST',
