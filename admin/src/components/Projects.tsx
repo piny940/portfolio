9675606c9fd604d112186a9a9a749a6188076269
piny940 <an.s.shoma@gmail.com>
2024-01-27 23:51:02 +0900

    projectsのitemをDNDできるようにする

--- admin/src/components/Projects.tsx
+++ admin/src/components/Projects.tsx
@@ -1,7 +1,12 @@
-import { useDeleteProjectMutation, useGetProjectsQuery } from '@/graphql/types'
+import {
+  Project,
+  useDeleteProjectMutation,
+  useGetProjectsQuery,
+} from '@/graphql/types'
 import {
   Box,
   Button,
+  IconButton,
   Table,
   TableBody,
   TableCell,
@@ -11,16 +16,54 @@ import {
 } from '@mui/material'
 import Error from 'next/error'
 import Link from 'next/link'
-import { useMemo } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { dateLabel } from '../../utils/helpers'
+import { CSS } from '@dnd-kit/utilities'
+import DragHandleIcon from '@mui/icons-material/DragHandle'
+import {
+  SortableContext,
+  arrayMove,
+  useSortable,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable'
+import { DndContext, DragEndEvent } from '@dnd-kit/core'
 
+type ProjectType = Pick<
+  Project,
+  | 'id'
+  | 'title'
+  | 'description'
+  | 'isFavorite'
+  | 'createdAt'
+  | 'updatedAt'
+  | 'position'
+>
 export const Projects = (): JSX.Element => {
   const context = useMemo(() => ({ additionalTypenames: ['Project'] }), [])
   const [{ data, error }] = useGetProjectsQuery({ context })
   const [, deleteProject] = useDeleteProjectMutation()
+  const [projects, setProjects] = useState<ProjectType[]>()
+
+  const onDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event
+    if (over && active.id !== over.id) {
+      setProjects((projects) => {
+        if (!projects) return
+        const ids = projects.map((project) => project.id)
+        const oldIndex = ids.indexOf(active.id as string)
+        const newIndex = ids.indexOf(over.id as string)
+        return arrayMove(projects, oldIndex, newIndex)
+      })
+    }
+  }
+
+  useEffect(() => {
+    if (!data) return
+    setProjects(data.projects)
+  }, [data])
 
   if (error) return <Error statusCode={400} />
-  if (!data) return <>loading...</>
+  if (!projects) return <>loading...</>
   return (
     <Box>
       <Typography variant="h4" component="h2">
@@ -36,58 +79,90 @@ export const Projects = (): JSX.Element => {
           新規作成
         </Button>
       </Box>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Id</TableCell>
-            <TableCell>Title</TableCell>
-            <TableCell>Description</TableCell>
-            <TableCell>IsFavorite</TableCell>
-            <TableCell>Position</TableCell>
-            <TableCell>CreatedAt</TableCell>
-            <TableCell>UpdatedAt</TableCell>
-            <TableCell>Links</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.projects.map((project) => (
-            <TableRow key={project.id}>
-              <TableCell>{project.id}</TableCell>
-              <TableCell>{project.title}</TableCell>
-              <TableCell>{project.description}</TableCell>
-              <TableCell>{project.isFavorite ? 'true' : 'false'}</TableCell>
-              <TableCell>{project.position}</TableCell>
-              <TableCell>{dateLabel(project.createdAt)}</TableCell>
-              <TableCell>{dateLabel(project.updatedAt)}</TableCell>
-              <TableCell
-                sx={{
-                  '> * + *': {
-                    marginLeft: 1,
-                  },
-                }}
-              >
-                <Button
-                  variant="contained"
-                  component={Link}
-                  size="small"
-                  href={`/projects/${project.id}/edit`}
-                >
-                  編集
-                </Button>
-                <Button
-                  variant="contained"
-                  size="small"
-                  onClick={() => {
+      <DndContext onDragEnd={onDragEnd}>
+        <SortableContext
+          strategy={verticalListSortingStrategy}
+          items={projects}
+        >
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell></TableCell>
+                <TableCell>Id</TableCell>
+                <TableCell>Title</TableCell>
+                <TableCell>Description</TableCell>
+                <TableCell>IsFavorite</TableCell>
+                <TableCell>Position</TableCell>
+                <TableCell>CreatedAt</TableCell>
+                <TableCell>UpdatedAt</TableCell>
+                <TableCell>Links</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {projects.map((project) => (
+                <ProjectItem
+                  key={project.id}
+                  project={project}
+                  deleteProject={() => {
                     void deleteProject({ id: project.id })
                   }}
-                >
-                  削除
-                </Button>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+                />
+              ))}
+            </TableBody>
+          </Table>
+        </SortableContext>
+      </DndContext>
     </Box>
+  )
+}
+
+type ProjectItemProps = {
+  project: ProjectType
+  deleteProject: () => void
+}
+const ProjectItem = ({ project, deleteProject }: ProjectItemProps) => {
+  const { listeners, setNodeRef, transform, transition, attributes } =
+    useSortable({
+      id: project.id,
+    })
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  }
+  return (
+    <TableRow ref={setNodeRef} style={style} {...attributes}>
+      <TableCell>
+        <IconButton {...listeners}>
+          <DragHandleIcon />
+        </IconButton>
+      </TableCell>
+      <TableCell>{project.id}</TableCell>
+      <TableCell>{project.title}</TableCell>
+      <TableCell>{project.description}</TableCell>
+      <TableCell>{project.isFavorite ? 'true' : 'false'}</TableCell>
+      <TableCell>{project.position}</TableCell>
+      <TableCell>{dateLabel(project.createdAt)}</TableCell>
+      <TableCell>{dateLabel(project.updatedAt)}</TableCell>
+      <TableCell
+        sx={{
+          '> *': {
+            marginLeft: 1,
+            marginTop: 1,
+          },
+        }}
+      >
+        <Button
+          variant="contained"
+          component={Link}
+          size="small"
+          href={`/projects/${project.id}/edit`}
+        >
+          編集
+        </Button>
+        <Button variant="contained" size="small" onClick={deleteProject}>
+          削除
+        </Button>
+      </TableCell>
+    </TableRow>
   )
 }
