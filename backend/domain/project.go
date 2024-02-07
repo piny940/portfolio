feeb22fc270a1574ee8a6cbaca31c0c2ebe54c89
piny940 <an.s.shoma@gmail.com>
2024-02-07 12:02:10 +0900

    projectのtitle, descriptionに文字数制限をかける

--- backend/domain/project.go
+++ backend/domain/project.go
@@ -1,6 +1,7 @@
 package domain
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -36,6 +37,36 @@ type IProjectRepo interface {
 	Delete(id string) (*Project, error)
 	ListTags(projectIds []string) ([]*Technology, error)
 	UpdateTags(projectId string, technologyIds []uint) ([]*Technology, error)
+}
+
+const (
+	MIN_TITLE_LENGTH       = 1
+	MAX_TITLE_LENGTH       = 20
+	MIN_DESCRIPTION_LENGTH = 1
+	MAX_DESCRIPTION_LENGTH = 80
+)
+
+type invalidTitleLengthError struct{}
+type invalidDescriptionLengthError struct{}
+
+func (e *invalidTitleLengthError) Error() string {
+	return fmt.Sprintf("title length must be between %d and %d", MIN_TITLE_LENGTH, MAX_TITLE_LENGTH)
+}
+func (e *invalidDescriptionLengthError) Error() string {
+	return fmt.Sprintf("description length must be between %d and %d", MIN_DESCRIPTION_LENGTH, MAX_DESCRIPTION_LENGTH)
+}
+
+func (p ProjectInput) IsTitleValid() error {
+	if 0 < len(p.Title) && len(p.Title) <= 20 {
+		return nil
+	}
+	return &invalidDescriptionLengthError{}
+}
+func (p ProjectInput) IsDescriptionValid() error {
+	if 0 < len(p.Description) && len(p.Description) <= 80 {
+		return nil
+	}
+	return &invalidDescriptionLengthError{}
 }
 
 func (p *Project) ToInput() ProjectInput {
