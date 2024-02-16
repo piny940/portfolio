777a7250e13606d78ada451f6fac10bdff5e2f0b
piny940 <an.s.shoma@gmail.com>
2024-02-17 00:03:40 +0900

    Profile, PieItemのcssをstylesに移す

--- app/src/components/Common/PieItem.tsx
+++ app/src/components/Common/PieItem.tsx
@@ -1,37 +1,5 @@
-import { ReactNode } from 'react'
-import styled from 'styled-components'
-
-const CircleDiv = styled.div`
-  width: 160px;
-  height: 160px;
-  background-color: white;
-  position: absolute;
-  border-radius: 50%;
-  top: 20px;
-  left: 20px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  font-size: 30px;
-`
-
-const PieDiv = styled.div<{
-  $start: number[]
-  $end: number[]
-  $percent: number
-}>`
-  width: 200px;
-  height: 200px;
-  border-radius: 50%;
-  background-image: conic-gradient(
-    ${(
-      props
-    ) => `rgb(${props.$start[0]}, ${props.$start[1]}, ${props.$start[2]}), rgb(${props.$end[0]}, ${props.$end[1]}, ${props.$end[2]}) ${props.$percent}%,
-    rgb(79, 79, 79) ${props.$percent}% 100%`}
-  );
-  position: relative;
-`
+import { ReactNode, useMemo } from 'react'
+import styles from '@/styles/pie-item.module.scss'
 
 export type PieItemProps = {
   percent: number
@@ -44,23 +12,25 @@ export type PieItemProps = {
 export const PieItem: React.FC<PieItemProps> = ({
   percent,
   children,
-  className,
+  className = '',
   innerClassName,
   testId,
 }) => {
-  const start = [0, 85, 255]
-  const end = [0, 255, 238]
-  const color = end.map((v, i) => start[i] + ((v - start[i]) * percent) / 100)
+  const bgImage = useMemo(() => {
+    const start = [0, 85, 255]
+    const end = [0, 255, 238]
+    const color = end.map((v, i) => start[i] + ((v - start[i]) * percent) / 100)
+    return `conic-gradient(rgb(${start[0]}, ${start[1]}, ${start[2]}), rgb(${color[0]}, ${color[1]}, ${color[2]}) ${percent}%,
+    rgb(79, 79, 79) ${percent}% 100%)`
+  }, [percent])
 
   return (
-    <PieDiv
+    <div
+      style={{ backgroundImage: bgImage }}
       data-testid={testId}
-      $start={start}
-      $end={color}
-      $percent={percent}
-      className={className}
+      className={`${className} ${styles.pieItem}`}
     >
-      <CircleDiv className={innerClassName}>{children}</CircleDiv>
-    </PieDiv>
+      <div className={innerClassName}>{children}</div>
+    </div>
   )
 }
