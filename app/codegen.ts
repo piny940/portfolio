cb2a88626306ade0294e4bdeda11a665143f663e
piny940 <an.s.shoma@gmail.com>
2024-02-18 22:47:13 +0900

    codegenを導入

---
+++ app/codegen.ts
@@ -0,0 +1,23 @@
+import type { CodegenConfig } from '@graphql-codegen/cli'
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: '../backend/schema/*.gql',
+  documents: 'src/**/*.gql',
+  generates: {
+    'src/server/_types.ts': {
+      preset: 'client',
+      plugins: ['typescript', 'typescript-operations', 'typescript-urql'],
+      config: {
+        strictScalars: true,
+        scalars: {
+          Time: 'string',
+          Uint: 'number',
+        },
+        withHooks: true,
+      },
+    },
+  },
+}
+
+export default config
