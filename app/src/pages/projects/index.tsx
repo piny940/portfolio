e98a45963c3077694796e9cfc1f6cbac50d93a72
piny940 <an.s.shoma@gmail.com>
2024-02-19 11:00:17 +0900

    container/projectsをpagesに移動

--- app/src/pages/projects/index.tsx
+++ app/src/pages/projects/index.tsx
@@ -1,22 +1,40 @@
-import Projects from '@/containers/Projects'
+import Breadcrumb from '@/components/Common/Breadcrumb'
+import ProjectItems from '@/components/Portfolio/ProjectItems'
 import Meta from '@/layouts/Meta'
-import { PortfolioData, loadPortfolioData } from '@/server/common'
+import { Project } from '@/server/_types'
+import { getProjects } from '@/server/api'
 
 type ProjectsProps = {
-  data: PortfolioData
+  projects: Project[]
 }
 
 export const getServerSideProps = async (): Promise<{
   props: ProjectsProps
 }> => ({
-  props: { data: await loadPortfolioData() },
+  props: {
+    projects: await getProjects(),
+  },
 })
 
-const ProjectsPage = ({ data }: ProjectsProps): JSX.Element => {
+const ProjectsPage = ({ projects }: ProjectsProps): JSX.Element => {
+  const paths = [
+    { name: 'トップページ', path: '/' },
+    { name: 'プロジェクト', path: '/projects' },
+  ]
+
   return (
     <>
       <Meta />
-      <Projects data={data} />
+      <div className="container pt-3">
+        <Breadcrumb paths={paths} />
+        <section
+          id="projects"
+          className="d-flex align-items-center flex-column row-gap-3 row-gap-md-5"
+        >
+          <h1 className="h1 title-underline px-1">プロジェクト</h1>
+          <ProjectItems projects={projects} />
+        </section>
+      </div>
     </>
   )
 }
