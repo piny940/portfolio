69fb6398c5e99982d991c285fa790aae96d8abd6
piny940 <an.s.shoma@gmail.com>
2024-03-09 15:45:31 +0900

    テストを修正

--- app/__tests__/contexts/ThemeProvider.test.tsx
+++ app/__tests__/contexts/ThemeProvider.test.tsx
@@ -6,25 +6,25 @@ import { renderHook, waitFor, act } from '@testing-library/react'
 describe('<ThemeProvider />', () => {
   it('正常にテーマを取得できる', async () => {
     const wrapper = ({ children }: { children: ReactNode }) => {
-      return <ThemeProvider>{children}</ThemeProvider>
+      return <ThemeProvider initialTheme="dark">{children}</ThemeProvider>
     }
     const { result } = renderHook(() => useTheme(), { wrapper: wrapper })
 
     await waitFor(() => {
-      expect(result.current.theme).toBe('light')
+      expect(result.current.theme).toBe('dark')
     })
   })
   it('正常にテーマを更新できる', async () => {
     const wrapper = ({ children }: { children: ReactNode }) => {
-      return <ThemeProvider>{children}</ThemeProvider>
+      return <ThemeProvider initialTheme="dark">{children}</ThemeProvider>
     }
     const { result } = renderHook(() => useTheme(), { wrapper: wrapper })
 
     act(() => {
-      result.current.setTheme('dark')
+      result.current.setTheme('light')
     })
     await waitFor(() => {
-      expect(result.current.theme).toBe('dark')
+      expect(result.current.theme).toBe('light')
     })
   })
 })
