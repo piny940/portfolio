af57fd0caa490f65e7ec391e2c9c87a97384972d
piny940 <83708535+piny940@users.noreply.github.com>
2024-05-11 22:54:24 +0900

    migrationはsqlでするようにする

--- backend/db/init.go
+++ backend/db/init.go
@@ -1,7 +1,6 @@
 package db
 
 import (
-	"backend/domain"
 	"fmt"
 	"os"
 
@@ -23,48 +22,9 @@ func Init() {
 	if err != nil {
 		panic(err)
 	}
-	if err = client.AutoMigrate(
-		&domain.Project{},
-		&domain.Blog{},
-		&domain.Technology{},
-		&BlogTechnologyTag{},
-		&ProjectTechnologyTag{},
-		&ProjectLink{},
-		&domain.TechStack{},
-	); err != nil {
-		panic(err)
-	}
 	db = &DB{Client: client.Debug()}
-	db.Migrate()
 }
 
 func GetDB() *DB {
 	return db
-}
-
-func (db *DB) Migrate() {
-	if err := db.Client.SetupJoinTable(&domain.Blog{}, "Tags", &BlogTechnologyTag{}); err != nil {
-		panic(err)
-	}
-	if err := db.Client.SetupJoinTable(&domain.Project{}, "Tags", &ProjectTechnologyTag{}); err != nil {
-		panic(err)
-	}
-	if err := db.Client.Migrator().DropConstraint(&ProjectLink{}, "fk_project_links_project"); err != nil {
-		panic(err)
-	}
-	if err := db.Client.Migrator().CreateConstraint(&ProjectLink{}, "fk_project_links_project"); err != nil {
-		panic(err)
-	}
-	if err := db.Client.Migrator().DropConstraint(&ProjectTechnologyTag{}, "fk_project_technology_tags_project"); err != nil {
-		panic(err)
-	}
-	if err := db.Client.Migrator().CreateConstraint(&ProjectTechnologyTag{}, "fk_project_technology_tags_project"); err != nil {
-		panic(err)
-	}
-	if err := db.Client.Migrator().DropConstraint(&BlogTechnologyTag{}, "fk_blog_technology_tags_blog"); err != nil {
-		panic(err)
-	}
-	if err := db.Client.Migrator().CreateConstraint(&BlogTechnologyTag{}, "fk_blog_technology_tags_blog"); err != nil {
-		panic(err)
-	}
 }
