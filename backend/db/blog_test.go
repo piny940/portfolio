ffe2d89cf787bb9b79f2557aa9b0bf43475e2b84
piny940 <83708535+piny940@users.noreply.github.com>
2024-05-13 12:02:07 +0900

    db/blogのテストを作成

---
+++ backend/db/blog_test.go
@@ -0,0 +1,174 @@
+package db
+
+import (
+	"backend/domain"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/bluele/factory-go/factory"
+	"github.com/maxatome/go-testdeep/td"
+)
+
+var blogF = factory.NewFactory(
+	&domain.BlogInput{
+		URL:         "https://example.com",
+		Kind:        int(domain.BlogKindQiita),
+		PublishedAt: time.Now().UTC(),
+	},
+).SeqInt("Title", func(n int) (interface{}, error) {
+	return fmt.Sprintf("Blog%d", n), nil
+})
+
+func createBlogTechnologyTag(t *testing.T, blogID uint, technologyID uint) error {
+	t.Helper()
+	now := time.Now().UTC()
+	result := db.Client.Exec(
+		"insert into blog_technology_tags (blog_id, technology_id, created_at, updated_at) values (?, ?, ?, ?)",
+		blogID,
+		technologyID,
+		now, now,
+	)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func TestCreateBlog(t *testing.T) {
+	setup(t)
+	repo := NewBlogRepo(db)
+	var beforeCount int
+	db.Client.Raw("select count(*) from blogs").Scan(&beforeCount)
+
+	blog := blogF.MustCreate().(*domain.BlogInput)
+	repo.Create(*blog)
+	var afterCount int
+	db.Client.Raw("select count(*) from blogs").Scan(&afterCount)
+	if afterCount != beforeCount+1 {
+		t.Errorf("blog not created")
+	}
+	var actual domain.Blog
+	result := db.Client.First(&actual)
+	if result.Error != nil || actual.Title != blog.Title {
+		t.Errorf("blog not found: %v", result.Error)
+	}
+}
+
+func TestListBlogs(t *testing.T) {
+	setup(t)
+	length := 3
+	repo := NewBlogRepo(db)
+	blogs := make([]*domain.Blog, length)
+	publishedAts := make([]time.Time, length)
+	publishedAts[0], _ = time.Parse("2006-01-02-15:04:05", "2021-01-02-00:00:00")
+	publishedAts[1], _ = time.Parse("2006-01-02-15:04:05", "2021-01-01-00:00:00")
+	publishedAts[2], _ = time.Parse("2006-01-02-15:04:05", "2021-01-03-00:00:00")
+	for i, publishedAt := range publishedAts {
+		blog := blogF.MustCreateWithOption(map[string]interface{}{"PublishedAt": publishedAt}).(*domain.BlogInput)
+		newBlog, err := repo.Create(*blog)
+		if err != nil {
+			t.Errorf("failed to create blog: %v", err)
+		}
+		blogs[i] = newBlog
+	}
+	actual, err := repo.List()
+	if err != nil {
+		t.Errorf("failed to list blogs: %v", err)
+	}
+	orderedBlogs := []*domain.Blog{blogs[2], blogs[0], blogs[1]}
+	td.Cmp(t, actual, orderedBlogs)
+}
+
+func TestListBlogTagsComplex(t *testing.T) {
+	setup(t)
+	length := 5
+	bRepo := NewBlogRepo(db)
+	tRepo := NewTechnologyRepo(db)
+	techs := make([]*domain.Technology, length)
+	for i := 0; i < length; i++ {
+		tech := technologyF.MustCreate().(*domain.TechnologyInput)
+		newTech, err := tRepo.Create(*tech)
+		if err != nil {
+			t.Errorf("failed to create technology: %v", err)
+		}
+		techs[i] = newTech
+	}
+	blogs := make([]*domain.Blog, length)
+	for i := 0; i < length; i++ {
+		blog := blogF.MustCreate().(*domain.BlogInput)
+		newBlog, _ := bRepo.Create(*blog)
+		blogs[i] = newBlog
+	}
+	for bi := 0; bi < 3; bi++ {
+		for ti := bi; ti < bi+2; ti++ {
+			createBlogTechnologyTag(t, blogs[bi].ID, techs[ti].ID)
+		}
+	}
+	var count int
+	db.Client.Raw("select count(*) from blog_technology_tags").Scan(&count)
+	tags, err := bRepo.ListTags([]uint{blogs[0].ID, blogs[1].ID})
+	if err != nil {
+		t.Errorf("should not fail: %v", err)
+	}
+	expected := []*domain.BlogTag{
+		{BlogID: blogs[0].ID, Technology: *techs[0]},
+		{BlogID: blogs[0].ID, Technology: *techs[1]},
+		{BlogID: blogs[1].ID, Technology: *techs[1]},
+		{BlogID: blogs[1].ID, Technology: *techs[2]},
+	}
+	if len(tags) != len(expected) {
+		t.Errorf("expected %d tags, got %d", len(expected), len(tags))
+	}
+	for i, tag := range tags {
+		if tag.BlogID != expected[i].BlogID {
+			t.Errorf("expected %d, got %d", expected[i].BlogID, tag.BlogID)
+		}
+		if tag.Technology.ID != expected[i].Technology.ID {
+			t.Errorf("expected %d, got %d", expected[i].Technology.ID, tag.Technology.ID)
+		}
+	}
+}
+
+func TestUpdateBlogTags(t *testing.T) {
+	setup(t)
+	length := 5
+	bRepo := NewBlogRepo(db)
+	tRepo := NewTechnologyRepo(db)
+	techs := make([]*domain.Technology, length)
+	for i := 0; i < length; i++ {
+		tech := technologyF.MustCreate().(*domain.TechnologyInput)
+		newTech, err := tRepo.Create(*tech)
+		if err != nil {
+			t.Errorf("failed to create technology: %v", err)
+		}
+		techs[i] = newTech
+	}
+	blog, _ := bRepo.Create(*blogF.MustCreate().(*domain.BlogInput))
+	t.Run("Add tags", func(t *testing.T) {
+		_, err := bRepo.UpdateTags(blog.ID, []uint{techs[0].ID, techs[1].ID})
+		if err != nil {
+			t.Errorf("should not fail: %v", err)
+		}
+		tags, _ := bRepo.ListTags([]uint{blog.ID})
+		if len(tags) != 2 {
+			t.Errorf("expected 2 tags, got %d", len(tags))
+		}
+		if tags[0].Technology.ID != techs[0].ID || tags[1].Technology.ID != techs[1].ID {
+			t.Errorf("expected %d and %d, got %d and %d", techs[0].ID, techs[1].ID, tags[0].Technology.ID, tags[1].Technology.ID)
+		}
+	})
+	t.Run("Remove tags", func(t *testing.T) {
+		_, err := bRepo.UpdateTags(blog.ID, []uint{techs[0].ID})
+		if err != nil {
+			t.Errorf("should not fail: %v", err)
+		}
+		tags, _ := bRepo.ListTags([]uint{blog.ID})
+		if len(tags) != 1 {
+			t.Errorf("expected 1 tag, got %d", len(tags))
+		}
+		if tags[0].Technology.ID != techs[0].ID {
+			t.Errorf("expected %d, got %d", techs[0].ID, tags[0].Technology.ID)
+		}
+	})
+}
