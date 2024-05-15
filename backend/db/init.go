6802301384c06e43b6a5bb81bd7c7ef5b1330927
piny940 <83708535+piny940@users.noreply.github.com>
2024-05-15 17:32:49 +0900

    DBが死んでいる場合でもサーバーは落とさず接続試行を繰り返すようにする

--- backend/db/init.go
+++ backend/db/init.go
@@ -3,6 +3,7 @@ package db
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -18,7 +19,7 @@ func Init() {
 	dsn := fmt.Sprintf("user=%s password=%s host=%s dbname=%s sslmode=%s",
 		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"),
 		os.Getenv("DB_NAME"), os.Getenv("DB_SSLMODE"))
-	client, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	client, err := connect(dsn)
 	if err != nil {
 		panic(err)
 	}
@@ -27,4 +28,33 @@ func Init() {
 
 func GetDB() *DB {
 	return db
+}
+
+const RETRY_INTERVAL = 5
+
+func connect(dsn string) (*gorm.DB, error) {
+	var client *gorm.DB
+	var err error
+	for {
+		client, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err != nil {
+			fmt.Printf("Failed to connect to database. Retrying in %d seconds.\n", RETRY_INTERVAL)
+			time.Sleep(RETRY_INTERVAL * time.Second)
+			continue
+		}
+		db, err := client.DB()
+		if err != nil {
+			fmt.Printf("Failed to access to database. Retrying in %d seconds.\n", RETRY_INTERVAL)
+			time.Sleep(RETRY_INTERVAL * time.Second)
+			continue
+		}
+		err = db.Ping()
+		if err != nil {
+			fmt.Printf("Failed to ping database. Retrying in %d seconds.\n", RETRY_INTERVAL)
+			time.Sleep(RETRY_INTERVAL * time.Second)
+			continue
+		}
+		break
+	}
+	return client, nil
 }
