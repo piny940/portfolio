429932541000d5241ebfffcee09460d78ebadaa1
piny940 <83708535+piny940@users.noreply.github.com>
2024-05-15 18:09:21 +0900

    DBの接続試行回数に上限を設ける

--- backend/db/init.go
+++ backend/db/init.go
@@ -3,6 +3,7 @@ package db
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 
 	"gorm.io/driver/postgres"
@@ -19,7 +20,11 @@ func Init() {
 	dsn := fmt.Sprintf("user=%s password=%s host=%s dbname=%s sslmode=%s",
 		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"),
 		os.Getenv("DB_NAME"), os.Getenv("DB_SSLMODE"))
-	client, err := connect(dsn)
+	tryLimit, err := strconv.Atoi(os.Getenv("DB_TRY_LIMIT"))
+	if err != nil {
+		tryLimit = 0
+	}
+	client, err := connect(dsn, tryLimit)
 	if err != nil {
 		panic(err)
 	}
@@ -32,10 +37,15 @@ func GetDB() *DB {
 
 const RETRY_INTERVAL = 5
 
-func connect(dsn string) (*gorm.DB, error) {
+func connect(dsn string, tryLimit int) (*gorm.DB, error) {
 	var client *gorm.DB
 	var err error
+	tryCount := 0
 	for {
+		if tryLimit > 0 && tryCount >= tryLimit {
+			return nil, fmt.Errorf("failed to connect to database %d times", tryCount)
+		}
+		tryCount += 1
 		client, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 		if err != nil {
 			fmt.Printf("Failed to connect to database. Retrying in %d seconds.\n", RETRY_INTERVAL)
