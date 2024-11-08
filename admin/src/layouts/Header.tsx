9c9853554cecc7c50d50b11bed5575879ff2dd1c
piny940 <83708535+piny940@users.noreply.github.com>
2024-11-08 13:11:37 +0900

    ログアウトボタンを配置

--- admin/src/layouts/Header.tsx
+++ admin/src/layouts/Header.tsx
@@ -1,14 +1,26 @@
 'use client'
-import { AppBar, Toolbar, Typography } from '@mui/material'
+import { useGetMeQuery } from '@/graphql/types'
+import { AppBar, Button, Toolbar, Typography } from '@mui/material'
 import Link from 'next/link'
+import { toStorage } from '../../utils/storage'
+import { BACKEND_JWT_TOKEN_KEY } from '../../utils/constants'
 
 export const Header = (): JSX.Element => {
+  const [{ data }] = useGetMeQuery()
   return (
     <AppBar position="sticky">
-      <Toolbar>
+      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
         <Link href="/" className="unstyled">
           <Typography variant="h6">PortfolioAdmin</Typography>
         </Link>
+        {data?.me && (
+          <Button
+            onClick={() => toStorage(BACKEND_JWT_TOKEN_KEY, '')}
+            color="inherit"
+          >
+            Logout
+          </Button>
+        )}
       </Toolbar>
     </AppBar>
   )
