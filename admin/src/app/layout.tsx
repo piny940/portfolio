14de953d0b3a47ed2ac74b2c73cd01fa390ab1e7
piny940 <83708535+piny940@users.noreply.github.com>
2024-11-08 17:18:02 +0900

    dynamic renderingにする

--- admin/src/app/layout.tsx
+++ admin/src/app/layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from 'react'
 import '../styles/globals.scss'
 import { Layout } from '@/layouts/AppLayout'
 
+export const dynamic = 'force-dynamic'
 export default function Page({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
