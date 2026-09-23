b281571dfc8897c08df1a7dc1d4a365f18bdccbf
piny940 <83708535+piny940@users.noreply.github.com>
2026-09-23 12:26:16 +0900

    使われていないTestIDを削除
    Signed-off-by: piny940 <83708535+piny940@users.noreply.github.com>

--- src/resources/TestID.ts
+++ src/resources/TestID.ts
@@ -1,10 +1,7 @@
 export enum TestID {
-  TEST = 'test',
-  APP = 'app',
   CUSTOM404 = 'custom404',
   CUSTOM500 = 'custom500',
   NAVBAR = 'navbar',
-  PROFILE_BUTTON = 'profile-button',
   BLOG_ITEM = 'blog-item',
   SKILL_ITEM = 'skill-item',
   PROJECT_ITEM = 'project-item',
