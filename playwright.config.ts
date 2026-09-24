af4d23e0da322f40c59ad08dca86b67b8d4a2f24
piny940 <83708535+piny940@users.noreply.github.com>
2026-09-24 14:48:44 +0900

    playwrightのCIでは1件failしたら即終了する
    Signed-off-by: piny940 <83708535+piny940@users.noreply.github.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,6 +19,8 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+  /* Stop after the first failure on CI */
+  maxFailures: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
